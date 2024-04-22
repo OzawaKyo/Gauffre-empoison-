c3ed38e4d32516f0b3563a78cebb4f0c28551e18
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-04-22 20:47:31 +0200

    Improve the design of the main menu

--- GaufreView.java
+++ GaufreView.java
@@ -1,55 +1,60 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GaufreView extends JPanel {
-    private GaufreModel model;
-    private boolean hoverMode = false;
-    private JLabel currentPlayerLabel;
+  private GaufreModel model;
+  private boolean hoverMode = false;
+  private JLabel currentPlayerLabel;
 
-    public GaufreView(GaufreModel model) {
-        this.model = model;
-        setPreferredSize(new Dimension(400, 300)); // Taille de la vue
+  public GaufreView(GaufreModel model) {
+    this.model = model;
+    setPreferredSize(new Dimension(800, 600)); // Taille de la vue
 
-        // Créer le label pour afficher le joueur courant
-        currentPlayerLabel = new JLabel("Joueur courant : " + model.getCurrentPlayer());
-        add(currentPlayerLabel, BorderLayout.NORTH);
+    // Créer le label pour afficher le joueur courant
+    currentPlayerLabel = new JLabel("Joueur courant : " + model.getCurrentPlayer());
+    add(currentPlayerLabel, BorderLayout.NORTH);
 
-    }
+  }
 
-    public void updateCurrentPlayerLabel() {
-        currentPlayerLabel.setText("Joueur courant : " + model.getCurrentPlayer());
-    }
+  public void updateCurrentPlayerLabel() {
+    currentPlayerLabel.setText("Joueur courant : " + model.getCurrentPlayer());
+  }
 
-    public void toggleHoverMode() {
-        hoverMode = !hoverMode;
-        repaint(); // Actualiser la vue
-    }
+  public void toggleHoverMode() {
+    hoverMode = !hoverMode;
+    repaint(); // Actualiser la vue
+  }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+  @Override
+  protected void paintComponent(Graphics g) {
 
-        super.paintComponent(g);
-        Gauffre[][] gaufre = model.getGaufre();
-        int cellWidth = getWidth() / gaufre[0].length;
-        int cellHeight = getHeight() / gaufre.length;
+    super.paintComponent(g);
+    Gauffre[][] gaufre = model.getGaufre();
+    int cellWidth = getWidth() / gaufre[0].length;
+    int cellHeight = getHeight() / gaufre.length;
 
-        for (int i = 0; i < gaufre.length; i++) {
-            for (int j = 0; j < gaufre[i].length; j++) {
-                if (gaufre[i][j].isEmpoisonnée()) {
-                    g.setColor(Color.GREEN); // Couleur pour la case empoisonnée
-                } else if (!gaufre[i][j].isCroquée()) {
-                    g.setColor(Color.WHITE); // Couleur pour les cases comestibles
-                } else if (gaufre[i][j].isCroquée()) {
-                    g.setColor(Color.GRAY); // Couleur pour les cases croquées
-                }
-                if (gaufre[i][j].isHovered() && !gaufre[i][j].isCroquée() && hoverMode) {
-                    g.setColor(Color.YELLOW); // Couleur pour la case survolée
-                }
-                g.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
-                g.setColor(Color.BLACK);
-                g.drawRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
-            }
+    for (int i = 0; i < gaufre.length; i++) {
+      for (int j = 0; j < gaufre[i].length; j++) {
+        if (gaufre[i][j].isEmpoisonnée()) {
+          g.setColor(Color.GREEN); // Couleur pour la case empoisonnée
+        } else if (!gaufre[i][j].isCroquée()) {
+          g.setColor(Color.WHITE); // Couleur pour les cases comestibles
+        } else if (gaufre[i][j].isCroquée()) {
+          g.setColor(Color.GRAY); // Couleur pour les cases croquées
         }
+        if (gaufre[i][j].isHovered() && !gaufre[i][j].isCroquée() && hoverMode) {
+          g.setColor(Color.YELLOW); // Couleur pour la case survolée
+        }
+        g.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+        g.setColor(Color.BLACK);
+        g.drawRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+      }
     }
+  }
 
 }
--- SelectModeView.java
+++ SelectModeView.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,28 +11,62 @@ import javax.swing.JPanel;
 
 public class SelectModeView extends JPanel {
   private GaufreController controller;
+  JButton buttonAI;
+  JButton buttonPlayer;
 
   public SelectModeView(GaufreModel model, GaufreController controller) {
     this.controller = controller;
-    setPreferredSize(new Dimension(400, 300)); // Taille de la vue
+    setPreferredSize(new Dimension(800, 600)); // Taille de la vue
 
-    JButton button1 = new JButton("Mode 0");
-    button1.addActionListener(new ActionListener() {
+    buttonAI = new JButton("Random AI");
+    buttonAI.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         controller.setGamemode(0);
       }
     });
 
-    JButton button2 = new JButton("Mode 1");
-    button2.addActionListener(new ActionListener() {
+    buttonPlayer = new JButton("2 players");
+    buttonPlayer.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         controller.setGamemode(1);
       }
     });
 
-    add(button1);
-    add(button2);
+    setLayout(null);
+    add(buttonAI);
+    add(buttonPlayer);
   }
 
-  // other methods...
+  @Override
+  protected void paintComponent(Graphics gRaw) {
+
+    Graphics2D g = (Graphics2D) gRaw;
+
+    super.paintComponent(g);
+
+    // Draw background
+    g.setColor(Color.BLACK);
+    g.fillRect(0, 0, getWidth(), getHeight());
+
+    // Draw title
+    g.setColor(Color.WHITE);
+    g.setFont(new Font("Arial", Font.BOLD, 25));
+    String title = "Le jeu de la gaufre empoisonnée";
+    int titleWidth = g.getFontMetrics().stringWidth(title);
+    int titleX = (getWidth() - titleWidth) / 2;
+    int titleY = getHeight() / 4;
+    g.drawString(title, titleX, titleY);
+
+    // Draw buttons
+    int buttonWidth = 200;
+    int buttonHeight = 50;
+    int buttonX = (getWidth() - buttonWidth) / 2;
+    int buttonY = getHeight() / 2;
+    int buttonSpacing = 20;
+
+    buttonAI.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
+    buttonPlayer.setBounds(buttonX, buttonY + buttonHeight + buttonSpacing, buttonWidth, buttonHeight);
+
+  }
+
 }
